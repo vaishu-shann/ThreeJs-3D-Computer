d67dcbf315fe130e0afad9e5e49ef331d2a20e11
vaishu-shann <ilukiabi@gmail.com>
2023-09-12 11:23:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Hero from './component/Hero';
+
+function App() {
+  return (
+    <div className="App">
+<Hero/>
+    </div>
+  );
+}
+
+export default App;
